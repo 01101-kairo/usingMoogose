10489660ded3ba8aa13afb68cf7aae4667e5ac0b
01101-kairo <kairomilhomem@gmail.com>
2024-05-18 13:07:56 -0300

    feat(models): add User model

---
+++ src/model/familia.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const conn = require('../../config/mongo')
+
+conn()
+const familiaSchema = mongoose.Schema({
+  nome:String,
+  idade: Number,
+  profissao: String,
+})
+const familiaModel = mongoose.model('familia', familiaSchema)
+
+module.exports = familiaModel
